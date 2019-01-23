abaca4ec04ac242e0e33d2c4114c33c59869ceaa
Will Kapcio <wkapcio@gmail.com>
2019-01-23 13:34:39 -0500

    Added mas, macOS update option

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# Update Homebrew and macOS
+# Forked from https://github.com/imwally/scripts/blob/master/whatsnew
 
-# homebrew
+# Homebrew
 echo "Checking homebrew packages..."
 brew update > /dev/null;
 new_packages=$(brew outdated --quiet)
@@ -28,16 +30,30 @@ fi
 echo "Cleaning up old homebrew packages..."
 brew cleanup > /dev/null;
 
+# Update Mac App Store apps
+# Done via https://github.com/mas-cli/mas
+echo "Upgrading macOS App Store apps..."
+mas_upgrade=$(mas upgrade | grep Everything)
+
+if [ "$mas_upgrade" != "Everything is up-to-date" ]; then
+	echo "Upgrading App Store apps..."
+else
+	echo "No App Store updates available."
+fi
+
 # macOS
 echo "Checking macOS updates..."
-software_update=$(softwareupdate -l | tail +4)
+mac_update=$(softwareupdate -l 2>&1 | head -1)
 
-if [ "$software_update" != "No new software available." ]; then
-	echo "Do you wish to install these updates?"
+if [ "$mac_update" != "No new software available." ]; then
+	echo "Updates found."
+	echo "Do you wish to install these updates? This will restart your Mac."
 	select yn in "Yes" "No"; do
 	    case $yn in
 		Yes ) echo "Downloading and installing macOS packages..."; softwareupdate -iaR | tail +5; break;;
 		No ) exit;;
 	    esac
 	done
+else
+	echo "No macOS updates available."
 fi
