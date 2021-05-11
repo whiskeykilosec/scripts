aa6a952850f87c1d6edf83ff71f914e6648ecbbb
Will Kapcio <18176030+whiskeykilo@users.noreply.github.com>
2021-05-11 16:39:22 -0400

    Npm update

--- update.sh
+++ update.sh
@@ -33,6 +33,7 @@ brew cleanup > /dev/null;
 # Node
 echo "Updating global node packages..."
 npm update -g > /dev/null;
+npm audit fix > /dev/null;
 
 # Update Mac App Store apps
 # Done via https://github.com/mas-cli/mas
